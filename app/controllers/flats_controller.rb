24ab7f1653094c2184dc4f01fe5bbfee0294866c
cbrudesala <cbrudesala@gmail.com>
2014-11-26 17:49:39 +0100

    validations

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,55 @@
 class FlatsController < ApplicationController
+	def index
+		@flats = Flat.all
+	end
+
+	def new
+		@flat = Flat.new
+	end
+
+	def create
+		@flat = Flat.new flat_params
+
+		if @flat.save
+			flash[:notice] = "Flat created successfully"
+			redirect_to flat_path(@flat.id)
+		else
+			flash[:alert] = "Something went wrong :("
+			render 'new'
+		end
+	end
+
+	def show
+		@flat = Flat.find params[:id]
+	end
+
+	def edit
+		@flat = Flat.find params[:id]
+	end
+
+	def update
+		@flat = Flat.find params[:id]
+
+		if @flat.update_attributes flat_params
+			redirect_to action: :show, controller: :flats, id: @flat.id
+		else
+			render 'flat'
+		end
+	end
+
+	def destroy
+		@flat = Flat.find params[:id]
+
+		if @flat.destroy
+			redirect_to action: 'index'
+		else
+			redirect_to :back
+		end
+	end
+
+	private
+
+	def flat_params
+		params.require(:flat).permit(:name,:email,:num_rooms,:num_baths,:pet,:max_price,:min_price,:photo)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
 	# has_many :availability_dates
 	# has_and_belongs_to many :areas
 
-	# validates :name, presence: true
-	# validates :email, presence: true
-	# validates :num_rooms, presence: true
-	# validates :num_baths, presence: true
-	# validates :pet, presence: true
-	# validates :max_price, presence: true
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :num_rooms, presence: true
+	validates :num_baths, presence: true
+	validates_inclusion_of :pet, :in => [true, false]
+	validates :max_price, presence: true
 
 
 end
